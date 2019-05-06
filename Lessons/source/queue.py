99f64dd3f5bc5bdcb312bf6c738a01f06d897a38
betsybaileyy <betsy.bailey80@gmail.com>
2019-05-06 15:49:06 -0700

    traversals recursive

--- Lessons/source/queue.py
+++ Lessons/source/queue.py
@@ -10,6 +10,7 @@ class LinkedQueue(object):
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any."""
         # Initialize a new linked list to store the items
+        self.size = 0
         self.list = LinkedList()
         if iterable is not None:
             for item in iterable:
@@ -31,7 +32,7 @@ class LinkedQueue(object):
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – Time here is constant because we are only dequeueing one item"""
         # Insert given item
         self.list.append(item)
 
@@ -47,11 +48,11 @@ class LinkedQueue(object):
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – time here is constant because we are only dequeueing one item"""
         # Remove and return front item, if any
         if self.is_empty():
             raise ValueError("look at this error")
-        
+
         item = self.list.head.data
         self.list.head = self.list.head.next
         self.list.size -= 1
@@ -65,6 +66,7 @@ class ArrayQueue(object):
     def __init__(self, iterable=None):
         """Initialize this queue and enqueue the given items, if any."""
         # Initialize a new list (dynamic array) to store the items
+        self.size = 0
         self.list = list()
         if iterable is not None:
             for item in iterable:
@@ -76,26 +78,33 @@ class ArrayQueue(object):
 
     def is_empty(self):
         """Return True if this queue is empty, or False otherwise."""
-        # TODO: Check if empty
+        # Check if empty
+        if self.list.size == 0:
+            return True
+        else:
+            return False
 
     def length(self):
         """Return the number of items in this queue."""
-        # TODO: Count number of items
+        # Count number of items
+        return self.list.size
 
     def enqueue(self, item):
         """Insert the given item at the back of this queue.
-        Running time: O(???) – Why? [TODO]"""
-        # TODO: Insert given item
+        Running time: O(1) – we are only inserting one item """
+        # Insert given item
+        self.list.append(item)
 
     def front(self):
         """Return the item at the front of this queue without removing it,
         or None if this queue is empty."""
         # TODO: Return front item, if any
 
+
     def dequeue(self):
         """Remove and return the item at the front of this queue,
         or raise ValueError if this queue is empty.
-        Running time: O(???) – Why? [TODO]"""
+        Running time: O(1) – time here is constant because we are only dequeueing one item"""
         # TODO: Remove and return front item, if any
 
 
