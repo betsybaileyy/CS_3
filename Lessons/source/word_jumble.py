068eb8f0e76e3eeab9f50f0f5ce315cc8ab1912c
betsybaileyy <betsy.bailey80@gmail.com>
2019-05-14 18:20:06 -0700

    scenario 2 working

---
+++ Lessons/source/word_jumble.py
@@ -0,0 +1,22 @@
+import ../../../../../usr/share/dict/words
+from itertools import permutations
+from set import Set
+
+
+with open
+
+permutations = permutations(['t', 'e', 'f', 'o', 'n'], 5)
+
+dict_array = []
+permutations_array = []
+
+# Looping through all possible combinations
+# of the jumbled word and adding them to an array
+for i in list(permutations):
+    permutations_array.append(i)
+
+'''need to remove the quotations and commas from the items
+in permutations_array so that it is one coherent word'''
+
+if with open("/usr/share/dict/words") as words:
+    words = words.read().split("\n")
