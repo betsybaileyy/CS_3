2772be975efb1275a9ba4805af31b5f001ba88fc
betsybaileyy <betsy.bailey80@gmail.com>
2019-05-19 20:02:35 -0400

    updated from pair programming with Marianna

--- Lessons/source/Call_Route_Project.py
+++ Lessons/source/Call_Route_Project.py
@@ -1,14 +1,11 @@
-'''Scenario 1: One-time route cost check
-You have a carrier route list with 100,000 (100K) entries (in arbitrary order)
-and a single phone number. How quickly can you find the cost of calling this number?'''
-# Check the book for number and get the price
-# Open the file put file info into a list
-# Loop through it
-# Check if the number makes the given number
-# if it does return the set second value
-# Else phone number not found
+'''
+Pair programmed with Marianna Campbell :) !
+This solution works with all three scenarios.
+'''
+
 
 #!python
+import time
 from pprint import pprint
 from hashtable import HashTable
 
@@ -20,7 +17,8 @@ def load_data():
     with open('../../project/data/route-costs-4.txt', 'r') as f:
         for line in f:
             prefix, price = line.split(',')
-            route_costs.append((prefix, price))
+            pprice = price.replace("\n", "")
+            route_costs.append((prefix, pprice))
     return route_costs
 
 def init_hashtable(route_costs):
@@ -49,10 +47,12 @@ def is_prefix_match_and_get_price(ht, phone_num):
     # print("Phone Number:", phone_num)
     #check if the phone number is in the HashTable
     if not phone_num:
-        raise ValueError('phone number does not exist')
+        # raise ValueError('phone number does not exist')
+        return 0
     if ht.contains(phone_num):
         # if yes, return the price
-        return print("Route Cost for {}: ${}".format(phone_num, ht.get(phone_num)))
+        # return print("Route Cost for {}: ${}".format(phone_num, ht.get(phone_num)))
+        return ht.get(phone_num)
     #if not, pop off the last digit of the phone number
     else:
         phone_num = phone_num[:-1]
@@ -66,11 +66,50 @@ def get_prices(phone_numbers, is_prefix_match_and_get_price, ht):
     for number in phone_numbers:
     #pass one phone number into the prefix match function
     #append price to the list
-        price_list.append(is_prefix_match_and_get_price(ht, phone_num))
+        price = is_prefix_match_and_get_price(ht, number)
+        with open('HELLO_route-costs.txt', 'a') as f:
+            f.write("%s, %s \n" % (number, str(price) ))
+        # price_list.append(is_prefix_match_and_get_price(ht, phone_num))
 
     return price_list
 
 
+def load_phone_nums():
+    """
+    Returns a list of phone numbers.
+    """
+    phone_numbers = []
+
+    with open('../../project/data/phone-numbers-3.txt', 'r') as f:
+        for line in f:
+            # print(line)
+            individual_phone_num = line.replace("\n", "")
+            phone_numbers.append(individual_phone_num)
+
+    return phone_numbers
+
+
+if __name__ == '__main__':
+    start = time.time()
+    route_costs = load_data()
+
+    ht = init_hashtable(route_costs)
+    phone_numbers = load_phone_nums()
+    price_list = get_prices(phone_numbers, is_prefix_match_and_get_price, ht)
+    print(get_prices(phone_numbers, is_prefix_match_and_get_price, ht))
+    print(price_list)
+    end = time.time()
+    print(end - start)
+    # print(is_prefix_match_and_get_price(ht, phone_numbers))
+    # print(load_phone_nums())
+
+#Psuedocode
+# Check the book for number and get the price
+# Open the file put file info into a list
+# Loop through it
+# Check if the number makes the given number
+# if it does return the set second value
+# Else phone number not found
 
     # you've got a phone do we have a prefix that matches it
     # contains
@@ -90,29 +129,3 @@ def get_prices(phone_numbers, is_prefix_match_and_get_price, ht):
 # open up the phone number file
     #read the first line, strip away the new line character
     #add each phone number to a list
-
-
-def load_phone_nums():
-    """
-    Returns a list of phone numbers.
-    """
-    phone_numbers = []
-
-    with open('../../project/data/phone-numbers-3.txt', 'r') as f:
-        for line in f:
-            # print(line)
-            individual_phone_num = line.replace("\n", "")
-            phone_numbers.append(individual_phone_num)
-
-    return phone_numbers
-
-
-if __name__ == '__main__':
-    route_costs = load_data()
-    # print("Phone Numbers and Prices:", route_costs)
-    # print("***")
-    ht = init_hashtable(route_costs)
-    phone_numbers = load_phone_nums()
-    print(get_prices(phone_numbers, is_prefix_match_and_get_price, ht))
-    # print(is_prefix_match_and_get_price(ht, phone_numbers))
-    # print(load_phone_nums())
--- Lessons/source/binarytree.py
+++ Lessons/source/binarytree.py
@@ -1,5 +1,6 @@
 #!python
-
+from queue import LinkedQueue
+import math
 
 class BinaryTreeNode(object):
 
@@ -16,17 +17,23 @@ class BinaryTreeNode(object):
     def is_leaf(self):
         """Return True if this node is a leaf (has no children)."""
         # Check if both left child and right child have no value
-        if node is not None:
-            if self.left is None and self.right is None:
-                return True
+        return self.left is None and self.right is None
+        # if self.left is None and self.right is None:
+        #     return True
+        # else:
+        #     return False
         # return ... and ...
 
     def is_branch(self):
         """Return True if this node is a branch (has at least one child)."""
         # Check if either left child or right child has a value
-        if node is not None:
-            if self.right is not None or self.left is not None:
-                return True
+
+        return self.right is not None or self.left is not None
+
+        # if self.right is not None or self.left is not None:
+        #     return True
+        # else:
+        #     return False
         # return ... or ..
 
     def height(self):
@@ -53,7 +60,7 @@ class BinarySearchTree(object):
         self.root = None
         self.size = 0
         if items is not None:
-
+            for item in items:
                 self.insert(item)
 
     def __repr__(self):
@@ -64,13 +71,16 @@ class BinarySearchTree(object):
         """Return True if this binary search tree is empty (has no nodes)."""
         return self.root is None
 
-    def height(self):
+    def height(self): # !!!!!!!
         """Return the height of this tree (the number of edges on the longest
         downward path from this tree's root node to a descendant leaf node).
         TODO: Best and worst case running time: ??? under what conditions?"""
         # Check if root node has a value and if so calculate its height
-        if self.root is not None:
+        # if self.root is not None:
+        #     return self.root.height()
+        if not self.is_empty():
             return self.root.height()
+        return 0
 
     def contains(self, item):
         """Return True if this binary search tree contains the given item.
@@ -93,14 +103,14 @@ class BinarySearchTree(object):
 
 
 
-    def insert(self, item, node):
+    def insert(self, item):
         """Insert the given item in order into this binary search tree.
         TODO: Best case running time: ??? under what conditions?
         TODO: Worst case running time: ??? under what conditions?"""
         # Handle the case where the tree is empty
         if self.is_empty():
             # Create a new root node
-            self.root = node
+            self.root = BinaryTreeNode(item)
             # Increase the tree size
             self.size += 1
             return self.size
@@ -109,17 +119,18 @@ class BinarySearchTree(object):
         parent = self._find_parent_node_recursive(item, self.root)
 
         # Check if the given item should be inserted left of parent node
-        if item < parent.data:
+        if parent and item < parent.data:
             # Create a new node and set the parent's left child
-            parent.left = new_node
+            parent.left = BinaryTreeNode(item)
 
         # Check if the given item should be inserted right of parent node
-        elif item > parent.data:
-            # TODO: Create a new node and set the parent's right child
-            parent.right = new_node
+        elif parent and item > parent.data:
+            # Create a new node and set the parent's right child
+            parent.right = BinaryTreeNode()
 
         # Increase the tree size
         self.size += 1
+        return
 
 
 
@@ -171,11 +182,11 @@ class BinarySearchTree(object):
         # Check if the given item is less than the node's data
         elif item < node.data:
             # Recursively descend to the node's left child, if it exists
-            return self._find_node_recursive(node.left)
+            return self._find_node_recursive(item, node.left)
 
         # Check if the given item is greater than the node's data
         elif item > node.data:
-            return self._find_node_recursive(node.right)
+            return self._find_node_recursive(item, node.right)
 
     def _find_parent_node_iterative(self, item):
         """Return the parent node of the node containing the given item
@@ -232,22 +243,22 @@ class BinarySearchTree(object):
         elif item < node.data:
             # Recursively descend to the node's left child, if it exists
             if node is not None:
-                return _find_parent_node_recursive(item, node=node.left, parent=node)
+                return self._find_parent_node_recursive(item, node=node.left, parent=node)
 
         # Check if the given item is greater than the node's data
         elif item > node.data:
             # Recursively descend to the node's right child, if it exists
             if node is not None:
-                return _find_parent_node_recursive(item, node=node.left, parent=node)
+                return self._find_parent_node_recursive(item, node=node.left, parent=node)
 
 
-    def delete(self, item):
-        """Remove given item from this tree, if present, or raise ValueError.
-        TODO: Best case running time: ??? under what conditions?
-        TODO: Worst case running time: ??? under what conditions?"""
-        # TODO: Use helper methods and break this algorithm down into 3 cases
-        # based on how many children the node containing the given item has and
-        # implement new helper methods for subtasks of the more complex cases
+    # def delete(self, item):
+    #     """Remove given item from this tree, if present, or raise ValueError.
+    #     TODO: Best case running time: ??? under what conditions?
+    #     TODO: Worst case running time: ??? under what conditions?"""
+    #     # TODO: Use helper methods and break this algorithm down into 3 cases
+    #     # based on how many children the node containing the given item has and
+    #     # implement new helper methods for subtasks of the more complex cases
 
     def items_in_order(self):
         """Return an in-order list of all items in this binary search tree."""
@@ -272,12 +283,12 @@ class BinarySearchTree(object):
         if node.right is not None:
             self._traverse_in_order_recursive(node.right, visit)
 
-    def _traverse_in_order_iterative(self, node, visit):
-        """Traverse this binary tree with iterative in-order traversal (DFS).
-        Start at the given node and visit each node with the given function.
-        TODO: Running time: ??? Why and under what conditions?
-        TODO: Memory usage: ??? Why and under what conditions?"""
-        # TODO: Traverse in-order without using recursion (stretch challenge)
+    # def _traverse_in_order_iterative(self, node, visit):
+    #     """Traverse this binary tree with iterative in-order traversal (DFS).
+    #     Start at the given node and visit each node with the given function.
+    #     TODO: Running time: ??? Why and under what conditions?
+    #     TODO: Memory usage: ??? Why and under what conditions?"""
+    #     # TODO: Traverse in-order without using recursion (stretch challenge)
 
     def items_pre_order(self):
         """Return a pre-order list of all items in this binary search tree."""
@@ -301,12 +312,12 @@ class BinarySearchTree(object):
         # Traverse right subtree, if it exists
             self._traverse_pre_order_recursive(node.right, visit)
 
-    def _traverse_pre_order_iterative(self, node, visit):
-        """Traverse this binary tree with iterative pre-order traversal (DFS).
-        Start at the given node and visit each node with the given function.
-        TODO: Running time: ??? Why and under what conditions?
-        TODO: Memory usage: ??? Why and under what conditions?"""
-        # TODO: Traverse pre-order without using recursion (stretch challenge)
+    # def _traverse_pre_order_iterative(self, node, visit):
+    #     """Traverse this binary tree with iterative pre-order traversal (DFS).
+    #     Start at the given node and visit each node with the given function.
+    #     TODO: Running time: ??? Why and under what conditions?
+    #     TODO: Memory usage: ??? Why and under what conditions?"""
+    #     # TODO: Traverse pre-order without using recursion (stretch challenge)
 
     def items_post_order(self):
         """Return a post-order list of all items in this binary search tree."""
@@ -330,12 +341,12 @@ class BinarySearchTree(object):
         # Visit this node's data with given function
             visit(node.data)
 
-    def _traverse_post_order_iterative(self, node, visit):
-        """Traverse this binary tree with iterative post-order traversal (DFS).
-        Start at the given node and visit each node with the given function.
-        TODO: Running time: ??? Why and under what conditions?
-        TODO: Memory usage: ??? Why and under what conditions?"""
-        # TODO: Traverse post-order without using recursion (stretch challenge)
+    # def _traverse_post_order_iterative(self, node, visit):
+    #     """Traverse this binary tree with iterative post-order traversal (DFS).
+    #     Start at the given node and visit each node with the given function.
+    #     TODO: Running time: ??? Why and under what conditions?
+    #     TODO: Memory usage: ??? Why and under what conditions?"""
+    #     # TODO: Traverse post-order without using recursion (stretch challenge)
 
     def items_level_order(self):
         """Return a level-order list of all items in this binary search tree."""
@@ -351,20 +362,22 @@ class BinarySearchTree(object):
         Start at the given node and visit each node with the given function.
         TODO: Running time: ??? Why and under what conditions?
         TODO: Memory usage: ??? Why and under what conditions?"""
-        # TODO: Create queue to store nodes not yet traversed in level-order
-        queue = ...
-        # TODO: Enqueue given starting node
-        ...
-        # TODO: Loop until queue is empty
-        while ...:
-            # TODO: Dequeue node at front of queue
-            node = ...
-            # TODO: Visit this node's data with given function
-            ...
-            # TODO: Enqueue this node's left child, if it exists
-            ...
-            # TODO: Enqueue this node's right child, if it exists
-            ...
+        # Create queue to store nodes not yet traversed in level-order
+        queue = LinkedQueue()
+        # Enqueue given starting node
+        queue.enqueue(start_node)
+        # Loop until queue is empty
+        while not queue.is_empty():
+            # Dequeue node at front of queue
+            node = queue.dequeue()
+            # Visit this node's data with given function
+            visit(node.data)
+            # Enqueue this node's left child, if it exists
+            if node.left:
+                queue.enqueue(node.left)
+            # Enqueue this node's right child, if it exists
+            if node.right:
+                queue.enqueue(node.right)
 
 
 def test_binary_search_tree():
